37aa1ee34511742a02cb8458b641029c84bae0db
mwong1015 <marcowong1015@gmail.com>
2023-03-13 17:22:31 +0800

    cipher

--- src/VigenereCipher.java
+++ src/VigenereCipher.java
@@ -66,32 +66,4 @@ public class VigenereCipher {
     public String getAlphabet() {
         return alphabet;
     }
-    // so set up a two dimension array for the possible
-    // then loop through and get the suitable column, add it to the string jesus this is hard
-
 }
-// cipher works by
-
-//    VigenereCipher vc = new VigenereCipher(“lemon”);
-//
-//    String encodedMessage = vc.encode(“attackatdawn”);
-//
-//System.out.println(encodedMessage); // lxfopvefrnhr
-//
-//        String decodedMessage = vc.decode(“lxfopvefrnhr”);
-//
-//        System.out.println(decodedMessage); // attackatdawn
-//
-//        String anotherEncodedMessage = vs.encode(“attack!atdawn”);
-//
-//        System.out.println(anotherEncodedMessage); // lxfopv!mhqlaz
-//
-//        String anotherDecodedMessage = vs.decode(“lxfopv!mhqlaz”);
-//
-//        System.out.println(anotherDecodedMessage); // attack!atdawn
-//
-//        Work with lowercase letters. That is, convert the key to lowercase when it is initialized, and the encoded or decoded message should only display lowercase letters.
-//
-//        String aFurtherEncodedMessage = vc.encode(“attackAtdaWn”);
-//
-//        System.out.println(aFurtherEncodedMessage); // lxfopvefrnhr
